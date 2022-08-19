e15bfbe08ee2281fc3d082b5e92535a607486150
k414bin <k414bin@yandex.ru>
2022-08-19 11:53:26 +0300

    Homework partial done

--- src/App.js
+++ src/App.js
@@ -1,41 +1,47 @@
 import './App.css';
 import React, { useState, useEffect,useCallback } from 'react';
 
+
 function App() {
-        
+           
     const [inputMessage,setInputMessage] = useState("");
     
     const [messagesArray,setMessagesArray] = useState([]);
     
     const onSendMessage = useCallback( () => {
-        setMessagesArray((prev) => [...prev,inputMessage]);     /* Не смог добавить автора сообщения */   
-        setInputMessage("");     
-    }, [inputMessage]);
-     
+           const trimmedMessageText = inputMessage.trim();
+           if  (trimmedMessageText !=="") {
+                    setMessagesArray((prev) => [...prev,trimmedMessageText]);     /* Не смог добавить автора сообщения */   
+                    setInputMessage("");     
+    }}, [inputMessage]);
+    
     useEffect(() => {
-    const chatBotTimeout = setTimeout(() => {                   
-          console.log("Chat-Bot answer...");          /* Я не смог ввести ответ в input, не разобрался как */
+       if (messagesArray.length > 0) {        
+            const chatBotTimeout = setTimeout(() => {                   
+                        let robotMessage = "Chat-Bot answer...";
+                        console.log( robotMessage );          /* Я не смог ввести ответ в input, не разобрался как */
     }, 1500);
     return () => clearTimeout(chatBotTimeout);
-  }, [messagesArray]);
-
-  return (
-    <div className="App">
-
-      <div className="mainWrapper">
+    }}, [messagesArray]);
   
+  
+  return (
+    <div className="App"> 
+   
+      <div className="mainWrapper">
+     
       <div className = "messageList">
       {messagesArray.map((message,i) => (
-        <div key = {i} >
-        <span className = "message"> {message} </span>
-        </div>
+        <div key = {i} >        
+        <span className = "message"> {message} </span>  
+        </div>        
         ))}
       </div>
       <div className = "inputWrapper">
       Введите сообщение >>
         <
-            input className = "input"             
-            value = {inputMessage} onChange = {(e) => setInputMessage(e.target.value)}
+            input className = "input"                 
+            required value = {inputMessage} onChange = {(e) => setInputMessage(e.target.value)}
             onKeyDown = {({key}) => {
                 if (key === 'Enter') {                 
                     onSendMessage();                
@@ -48,5 +54,6 @@ function App() {
     </div>
   );
 }
+
 
 export default App;
