78179a24ba359a33fdd6828dc0e9be4833f7fa2a
k414bin <k414bin@yandex.ru>
2022-08-26 17:56:38 +0300

    Chats forgotten code added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { Routes, Route, Navigate, Link } from "react-router-dom";
+import {Routes, Route, Navigate, Link, useParams} from "react-router-dom";
 import "./App.css";
 import {useCallback, useEffect, useState} from "react";
 import MessageList from "./Chat/MessageList";
@@ -69,10 +69,35 @@ function Home() {
 }
 
 function Chats() {
+    const {chatID} = useParams();
+    console.log(chatID);
+
+    const [chats,setChats]= useState([
+        {
+            id:'chat1',
+            messages:["сообщение №1"]
+        },
+        {
+            id:'chat2',
+            messages: ["сообщение №1","сообщение №2","сообщение №3"]
+        },
+    ])
+    const id = chats.findIndex( x => x.id === chatID);
+
     return (
         <>
-            <main>
             <h2 className="text-3xl font-bold underline">The page of chats.</h2>
+            <main style={{height: '50vh', background:'#51AFE',width:'100%'  }  } >
+                {/*НЕ работает. Я использовал MessageList для этого, но что-то пошло не так... */}
+                    <div>
+                        {
+                            chatID && chats[id]?.messages.map(e =>
+                            {return (
+                                <h2>{e}</h2>,
+                             <MessageList messagesArray={chats} />
+                            )})
+                        }
+                    </div>
             <nav>
                 <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center space-x-4">
                 <Link to={"/home"}>Home</Link>
@@ -80,9 +105,7 @@ function Chats() {
                 <Link to={'/chats/chat2'}>Chat2</Link>
                 </div>
             </nav>
-
                 <Messages />
-
             </main>
         </>
     );
