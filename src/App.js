6e4e38baf786c77959e541dae863c848a42f4cd1
k414bin <k414bin@yandex.ru>
2022-08-29 14:54:10 +0300

    First page of router done

--- src/App.js
+++ src/App.js
@@ -1,53 +1,10 @@
 import './App.css';
-import { Button } from "@material-tailwind/react";
-import {useState} from 'react';
 import Routed from './Routing/routed';
-import * as PropTypes from "prop-types";
-
-const ChildWithProps = ({state}) => {
-    return (
-        state ?
-            <h1 style={{color: '#FF0000'}}>Красный</h1> : <h1 style={{color: '#0000FF'}}>Синий</h1>
-    );
-}
-ChildWithProps.propTypes = {state: PropTypes.bool};
-
-const Modal = ({children,setModal}) => {
-    const styles = {
-        width: '50%',
-        height: '50%',
-        background: '#fff'
-    }
-
-    return (
-        <div className='modalContainer' onClick={(e) =>{setModal(true)}}>
-            <div style={styles} onClick={(e) =>{e.stopPropagation()}}>
-                {children}
-            </div>
-        </div>
-
-    );
-
-}
-
-Modal.propTypes = {
-    setModal: PropTypes.func,
-    children: PropTypes.node
-};
 
 function App () {
-    const [color, setColor] = useState(false);
-    const [modal, setModal] = useState(false);
+
     return (
         <div className="App">
-            <Button  className="bg-green-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={()=>{setModal(true)}}>Открыть профиль</Button>
-            {
-                modal === true && <Modal setModal={setModal}>
-                    <div> Modal body </div>
-                </Modal>
-            }
-            <Button  className="bg-blue-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded" onClick={()=>{setColor(prev => !prev)}}> Переключить цвет</Button>
-            <ChildWithProps state={color}></ChildWithProps>
             <Routed />
         </div>
     );
--- src/Routing/routed.js
+++ src/Routing/routed.js
@@ -4,20 +4,21 @@ import store from "../store";
 
 function Chats() {
     return null;
+
 }
 
 export default function Routed() {
  return (
          <BrowserRouter>
-             <div style={{width: '100px',display:'flex'}}>
+             <div className=" left top grid grid-cols-3 divide-x-4 divide-gray-500 divide-dots">
+                <div></div>
                 <Link className="text-lg text-blue-600 hover:underline hover:text-rose-900 visited:text-rose-700 active:text-yellow-500" to='/home'>Home</Link>
                 <Link className="text-lg text-blue-600 hover:underline hover:text-rose-900 visited:text-rose-700 active:text-yellow-500" to='/profile'> Profile </Link>
                 <Link className="text-lg text-blue-600 hover:underline hover:text-rose-900 visited:text-rose-700 active:text-yellow-500" to='/chats'>Chats</Link>
                 <Link className="text-lg text-blue-600 hover:underline hover:text-red-900 visited:text-rose-700 active:text-yellow-500 " to='/chats/chat1'>Chat1</Link>
                 <Link className="text-lg text-blue-600 hover:underline hover:text-red-900 visited:text-rose-700 active:text-yellow-500 " to='/chats/chat2'>Chat2</Link>
-                {/* <Link className="underline text-blue-600 hover:text-rose-900 visited:text-rose-700" to='/'>Home</Link>*/}
             </div>
-            <div style={{width: '1000px',display:'flex'}}>
+            <div>
                     <Routes>
                         <Route path="/profile" element={<Profile />} />
                         <Route path="/home" element={<Home />} />
@@ -60,7 +61,7 @@ function Profile() {
     }, [dispatch]);
 
     return (
-             <div>
+             <div >
                    <h4>Profile</h4>
                      <label htmlFor="small-input"
                             className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300">First name</label>
